d3f14b85d693a4c9d0eb99e99b57db9df272e626
CW-Khristos <khristos666@gmail.com>
2020-05-10 09:07:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
 --   Copyright 2019 ©oMarkez. All rights reserved    --
 -------------------------------------------------------
 */
+/* DEPENDENCIES */
 const Discord = require("discord.js");
 const mysql = require("mysql");
 const config = require("./config.json");
@@ -18,15 +19,16 @@ const lang = config.info.lang
 
 bot.login(token)
 
+/* MYSQL CONNECTION */
 var connection = mysql.createConnection({
     host     : config.info.database.host,
     user     : config.info.database.user,
     password : config.info.database.password,
     database : config.info.database.database
 });
-
 connection.connect();
 
+/* COMMAND : SQL QUERY TYPES */
 types = {
     "identity" : "SELECT * FROM vrp_user_identities WHERE user_id = ",
     "moneys" : "SELECT * FROM vrp_user_moneys WHERE user_id = ",
@@ -35,10 +37,12 @@ types = {
     "server" : "SELECT * FROM vrp_srv_data"
 }
 
+/* ADMIN COLLECTION */
 admins = {
     "discordidhere" : "namehere"
 }
 
+/* REPLY TO NON-ADMIN */
 function noAdmin(message, userid){
     const returnData = {
         color: 0x1207eb,
@@ -61,6 +65,7 @@ function noAdmin(message, userid){
     message.reply({embed: returnData})
 }
 
+/* QUERY MYSQL DATA */
 function getData(type, userid, cb){
     if(types[type]){
         var sql = types[type]
@@ -78,6 +83,7 @@ function getData(type, userid, cb){
     }
 }
 
+/* QUERY USER STATS */
 function returnData(message,id){
     connection.query("SELECT * FROM vrp_user_ids WHERE identifier LIKE '%" + id.toString() + "';", function(error, result, fields){
         if(error) throw error;
@@ -143,6 +149,7 @@ function returnData(message,id){
     })
 }
 
+/* QUERY USER CARS */
 function returnBiler(message, id){
     connection.query("SELECT * FROM vrp_user_ids WHERE identifier LIKE '%" + id.toString() + "';", function(error, result, fields){
         if(error) throw error;
@@ -175,6 +182,7 @@ function returnBiler(message, id){
     })
 }
 
+/* QUERY USER INVENTORY */
 function returnInventory(message, id){
     connection.query("SELECT * FROM vrp_user_ids WHERE identifier LIKE '%" + id.toString() + "';", function(error, result, fields){
         if(error) throw error;
@@ -221,6 +229,7 @@ function returnInventory(message, id){
     })
 }
 
+/* QUERY SERVER STATUS */
 function returnServer(message, id){
     connection.query("SELECT * FROM vrp_srv_data;", function(error, result, fields){
         if(error){
@@ -269,7 +278,9 @@ function returnServer(message, id){
     })
 }
 
+/* WRITE TO BOT CONSOLE */
 function writeConsole(message){
+    /* ASSIGN DISCORD USER ID */
     if(!message.mentions.users.first()){
         try{
             var userid = message.member.user.id
@@ -283,19 +294,24 @@ function writeConsole(message){
         catch(e){
         }
     }
+    /* USER IS ADMIN */
     if (admins[userid]){
         console.log("Query From : ADMIN : " + admins[userid])
+    /* USER IS NOT ADMIN */
     } else{
         console.log("Query From : USER : " + message.member.user.username)
     }
+    /* WRITE TO BOT CONSOLE */
     console.log("Message Text : " + message.content)
 }
 
+/* BOT EVENTS */
 bot.on("ready", () => {
   console.log('Bot is now Connected.')
 })
 
 bot.on("message", function(message){
+    /* ASSIGN DISCORD USER ID */
     if(!message.mentions.users.first()){
         try{
             var userid = message.member.user.id
@@ -309,20 +325,26 @@ bot.on("message", function(message){
         catch(e){
         }
     }
+    /* USER IS ADMIN */
     if (admins[userid]){
+        /* STATS */
         if(message.content.startsWith(prefix + config[lang].commands.stats)){
             writeConsole(message)
             returnData(message, userid)
+        /* CARS */
         } else if(message.content.startsWith(prefix + config[lang].commands.cars)){
             writeConsole(message)
             returnBiler(message, userid)
+        /* INVENTORY */
         } else if(message.content.startsWith(prefix + config[lang].commands.inventory)){
             writeConsole(message)
             returnInventory(message, userid)
+        /* SERVER */
         } else if(message.content.startsWith(prefix + config[lang].commands.server)){
             writeConsole(message)
             returnServer(message, userid)
         }
+    /* USER IS NOT ADMIN */
     } else{
         if(message.content.startsWith(prefix + config[lang].commands.server)){
             writeConsole(message)
